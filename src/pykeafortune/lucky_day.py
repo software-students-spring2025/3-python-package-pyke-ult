8416169a6d5eff4741767925c6dd59de5919b48e
larryyang04 <larryyang2848@gmail.com>
2025-03-19 20:08:52 -0400

    implemented lucky_day module and associated unit tests

--- src/pykeafortune/lucky_day.py
+++ src/pykeafortune/lucky_day.py
@@ -1,1 +1,48 @@
-# Lucky day file
+"""
+Lucky Day module:
+
+This module returns a lucky day based on the user's favorite day of the week.
+"""
+
+import datetime
+import random
+
+def get_lucky_day(favorite_day: str) -> datetime.datetime:
+    """
+    Returns a random future date that occurs on the user's favorite day of the week.
+
+    Parameters:
+        favorite_day (str): The user's favorite day of the week (e.g. Monday, Tuesday)
+
+    Returns:
+        datetime.date: A datetime.date object for the lucky day.
+
+    Raises:
+        ValueError: If the user's favorite day is not a valid day of the week.
+    """
+
+    favorite_day = favorite_day.lower()
+    days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+    if favorite_day not in days:
+        raise ValueError("Invalid day entered. Please enter a valid "
+                        "day of the week without any abbreviations.")
+
+    today = datetime.date.today()
+
+    current_day = today.weekday()
+    target_day = days.index(favorite_day)
+
+    day_offset = (target_day - current_day) % 7
+
+    # Generated lucky day must be in the future
+    if day_offset == 0:
+        day_offset += 7
+
+    # Generated lucky day occurs within the next 11 weeks,
+    # as week_range * 7 + day_offset <= 77
+    week_range = random.randint(0, 10)
+
+    lucky_day = today + datetime.timedelta(days=day_offset + (week_range * 7))
+
+    return lucky_day
--- tests/test_lucky_day.py
+++ tests/test_lucky_day.py
@@ -1,1 +1,91 @@
-# lucky day test file
+"""
+Unit tests for the lucky_day module.
+
+Verifies:
+    Correctness of generated lucky day.
+    Case-insensitivity of logic.
+    Generated lucky day falls within the allowed timeframe.
+    Handling of invalid user inputs.
+    Generated lucky day is at least a week in the future if favorite_day is today
+"""
+
+import datetime
+import pytest
+from pykeafortune import lucky_day
+
+def test_valid_lucky_day():
+    """
+    Verify get_lucky_day returns a valid lucky day in the 
+    future given valid user input.
+    """
+
+    favorite_day = "Monday"
+    result = lucky_day.get_lucky_day(favorite_day)
+
+    assert isinstance(result, datetime.date)
+    assert result > datetime.date.today()
+    assert result.weekday() == 0
+
+def test_case_insensitive():
+    """
+    Verify get_lucky_day returns a valid lucky day in the future 
+    regardless of capitalization of user input.
+    """
+
+    result_one = lucky_day.get_lucky_day("monday")
+    result_two = lucky_day.get_lucky_day("MONDAY")
+    result_three = lucky_day.get_lucky_day("MoNdAy")
+
+    assert isinstance(result_one, datetime.date)
+    assert isinstance(result_two, datetime.date)
+    assert isinstance(result_three, datetime.date)
+
+    assert result_one > datetime.date.today()
+    assert result_two > datetime.date.today()
+    assert result_three > datetime.date.today()
+
+    assert result_one.weekday() == 0
+    assert result_two.weekday() == 0
+    assert result_three.weekday() == 0
+
+def test_valid_timeframe():
+    """
+    Verify get_lucky_day returns a lucky day within the next
+    11 weeks, which is the allowed time frame.
+    """
+
+    result = lucky_day.get_lucky_day("Monday")
+
+    today = datetime.date.today()
+    date_boundary = today + datetime.timedelta(weeks=11)
+
+    assert today < result <= date_boundary
+
+def test_invalid_day():
+    """
+    Verify that get_lucky_day raises a ValueError upon receiving
+    an invalid day.
+    """
+
+    invalid = ["mon", "Th", 1, "", "mondaymonday", "FR1day"]
+
+    for day in invalid:
+        with pytest.raises(ValueError, match="Invalid day entered. "
+            "Please enter a valid day of the week without any abbreviations."):
+            lucky_day.get_lucky_day(day)
+
+def test_favorite_day_today():
+    """
+    Verify that if user's favorite day is today, then get_lucky_day returns a date
+    at least a week in the future.
+    """
+
+    days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+    today = datetime.date.today()
+    current_day_index = today.weekday()
+    current_day = days[current_day_index]
+
+    result = lucky_day.get_lucky_day(current_day)
+
+    assert result >= today + datetime.timedelta(days=7)
